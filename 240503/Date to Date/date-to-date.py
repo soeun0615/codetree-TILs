5a6daa15d264ada869148bfa78a11098e68bbf7b
soeun0615 <68493088+soeun0615@users.noreply.github.com>
2024-05-03 19:42:19 +0900

    Title: Date to Date, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 240503/Date to Date/date-to-date.py
+++ 240503/Date to Date/date-to-date.py
@@ -1,18 +1,16 @@
 num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 m1, d1, m2, d2 = map(int, input().split())
 
-gap1 = 0
-for i in range(m1):
-    gap1 += num_of_days[i-1]
+def get_days(month, day):
+    result = 0
+    for i in range(1, month):
+        result += num_of_days[i]
+    result += day
 
-gap1 += d1
+    return result
 
-gap2 = 0
-for i in range(m2):
-    gap2 += num_of_days[i-1]
+gap1 = get_days(m1, d1)
+gap2 = get_days(m2, d2)
+gap = gap2-gap1+1
 
-gap2 += d2
-
-gap = gap2-gap1
-if gap ==0: gap = 1
 print(gap)
