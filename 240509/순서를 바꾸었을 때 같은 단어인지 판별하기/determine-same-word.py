fd65baf8fbe6c6196499cefcc35abd5624210c9c
soeun0615 <68493088+soeun0615@users.noreply.github.com>
2024-05-09 15:20:02 +0900

    Title: 순서를 바꾸었을 때 같은 단어인지 판별하기, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240509/순서를 바꾸었을 때 같은 단어인지 판별하기/determine-same-word.py
@@ -0,0 +1,26 @@
+A = input()
+arr_A = list(A)
+arr_A.sort()
+
+B = input()
+arr_B = list(B)
+arr_B.sort()
+
+#순서를 바꿔서 동일한 단어로 만들려면
+# 1. 문자의 전체 수가 같아야하고
+# 2, 문자 종류와 수가 같아야함
+
+if len(arr_A) == len(arr_B):
+    for i in range(len(A)):
+        #print("i = ", i)
+        if arr_A[i] == arr_B[i]:
+            result = 0
+            pass
+        else:
+            result = 1
+            break
+
+    if result == 0:
+        print("Yes")
+    else:
+        print("No")
