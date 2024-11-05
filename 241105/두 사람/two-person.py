4178f1a059b6979295fb99fffbb2464d01587928
soeun0615 <68493088+soeun0615@users.noreply.github.com>
2024-11-05 22:47:28 +0900

    Title: 두 사람, Time: 40ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 241105/두 사람/two-person.py
@@ -0,0 +1,9 @@
+one = list(input().split())
+two = list(input().split())
+
+if int(one[0])>=19 and one[1]=='M':
+    print(1)
+elif int(two[0])>=19 and two[1]=='M':
+    print(1)
+else:
+    print(0)
