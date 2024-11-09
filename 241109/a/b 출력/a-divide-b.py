31a6cccef3d8b3457f3b5926406feb0db0f855bc
soeun0615 <68493088+soeun0615@users.noreply.github.com>
2024-11-09 18:44:04 +0900

    Title: a/b 출력, Time: 43ms, Memory: 15MB, Status: Passed - CodeTree

--- 241109/a/b 출력/a-divide-b.py
+++ 241109/a/b 출력/a-divide-b.py
@@ -1,10 +1,16 @@
-import math
 a, b = map(int, input().split())
 
-ans = []
-print(f'{a//b}.', end='')
-for _ in range(20):
-    a*=10
-    ans.append(a//b)
+# 정수 부분을 먼저 출력합니다.
+print(f"{a//b}.", end="")
 
-print(ans[-1])
+# a를 b로 나눈 나머지를 시작으로
+# 소수점 자리를 하나씩 계산합니다.
+a %= b
+for _ in range(20):
+    # 나머지에 10 곱한 값을 기준으로
+    # b로 나누었을 떄의 몫을 구해줍니다.
+    a *= 10
+    print(a // b, end="")
+
+    # a를 b로 나눈 나머지를 게속 갱신해줍니다.
+    a %= b
