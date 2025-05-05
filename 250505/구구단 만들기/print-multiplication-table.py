10fcd53a48b07350862af35b861dfe605198acbd
SE <68493088+soeun0615@users.noreply.github.com>
2025-05-05 16:31:50 +0900

    Title: 구구단 만들기, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250505/구구단 만들기/print-multiplication-table.py
+++ 250505/구구단 만들기/print-multiplication-table.py
@@ -7,8 +7,7 @@ for i in range(b, a-1, -1):
 
 for i in range(9):
     for j in c:
-        if (j%2==0):
-            print(f"{j} * {i+1} = {j * (i+1)}", end=" ")
+        print(f"{j} * {i+1} = {j * (i+1)}", end=" ")
 
         if (j!=c[len(c)-1]):
             print("/", end=" ")
