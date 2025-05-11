c11b9c279912e5861f812ee413dc3ad3fb3b8cf2
SE <68493088+soeun0615@users.noreply.github.com>
2025-05-11 21:32:16 +0900

    Title: 배열의 평균, Time: 48ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250511/배열의 평균/ave-of-array.py
@@ -0,0 +1,18 @@
+avg_h = []
+avg_v = []
+
+arr = [list(map(int, input().split())) for _ in range(2)]
+
+for i in range(2):
+    avg_h.append(sum(arr[i])/len(arr[i]))
+
+for j in range(4):
+    sum_v = 0
+    for i in range(2):
+        sum_v += arr[i][j]
+    avg_v.append(sum_v/len(arr))
+
+w_avg = (sum(avg_h)+sum(avg_v))/(len(arr[0])+len(arr))
+print(*avg_h)
+print(*avg_v)
+print(w_avg)
