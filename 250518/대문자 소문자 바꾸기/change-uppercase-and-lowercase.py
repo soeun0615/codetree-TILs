aa6619370a95983ca27bf9a291d10cf8c2dc44e1
SE <68493088+soeun0615@users.noreply.github.com>
2025-05-18 22:33:47 +0900

    Title: 대문자 소문자 바꾸기, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250518/대문자 소문자 바꾸기/change-uppercase-and-lowercase.py
+++ 250518/대문자 소문자 바꾸기/change-uppercase-and-lowercase.py
@@ -2,7 +2,7 @@ given = input()
 
 gap = ord('a') - ord('A')
 for elem in given:
-    if (ord(elem) > ord('a')) & (ord(elem) < ord('z')):
+    if (ord(elem) >= ord('a')) & (ord(elem) <= ord('z')):
         print(chr(ord(elem) - gap), end="")
     else:
         print(chr(ord(elem) + gap), end="")
