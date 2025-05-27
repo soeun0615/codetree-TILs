32daa9f4e2ff653b8b201409b74df414d0ddb3c7
SE <68493088+soeun0615@users.noreply.github.com>
2025-05-27 19:56:34 +0900

    Title: 100으로 나눈 나머지의 수열, Time: 66ms, Memory: 18MB, Status: Passed - Codetree

---
+++ 250527/100으로 나눈 나머지의 수열/sequence-of-remainder-divided-by-100.py
@@ -0,0 +1,13 @@
+N = int(input())
+
+# Please write your code here.
+def f(n):
+    if (n == 1):
+        return 2
+    if (n == 2):
+        return 4
+    
+    return (f(n-1) * f(n-2)) % 100
+
+
+print(f(N))
