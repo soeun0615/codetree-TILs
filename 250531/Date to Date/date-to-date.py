46737131a9358da2129be2166507cee3d15688bb
SE <68493088+soeun0615@users.noreply.github.com>
2025-05-31 15:36:57 +0900

    Title: Date to Date, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250531/Date to Date/date-to-date.py
@@ -0,0 +1,9 @@
+m1, d1, m2, d2 = map(int, input().split())
+
+# Please write your code here.
+days_of_month = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+days_1 = sum(days_of_month[:m1-1]) + d1
+days_2 = sum(days_of_month[:m2-1]) + d2
+
+print(abs(days_1 - days_2))
