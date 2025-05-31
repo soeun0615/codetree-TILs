1e53a9c2bb500ff8d52f7e6cadf3c0ebdc3bec1b
SE <68493088+soeun0615@users.noreply.github.com>
2025-05-31 15:42:57 +0900

    Title: Date to Date, Time: 41ms, Memory: 16MB, Status: Passed - Codetree

--- 250531/Date to Date/date-to-date.py
+++ 250531/Date to Date/date-to-date.py
@@ -3,10 +3,10 @@ m1, d1, m2, d2 = map(int, input().split())
 # Please write your code here.
 days_of_month = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-days_1 = sum(days_of_month[:m1-1]) + d1
-days_2 = sum(days_of_month[:m2-1]) + d2
+days_1 = sum(days_of_month[:m1]) + d1
+days_2 = sum(days_of_month[:m2]) + d2
 
-ans = abs(days_1 - days_2)
-if (ans == 0):
-    ans = 1
+ans = abs(days_1 - days_2) + 1
+
+# print(days_1, days_2)
 print(ans)
