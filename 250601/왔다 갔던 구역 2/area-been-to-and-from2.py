8d205a8769e7140f2037b3f4b99013046fbcc666
SE <68493088+soeun0615@users.noreply.github.com>
2025-06-01 22:23:08 +0900

    Title: 왔다 갔던 구역 2, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250601/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 250601/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -9,8 +9,14 @@ for _ in range(n):
 # Please write your code here.
 ans = [0] * sum(x) * 2
 now = sum(x)
-# print(now)
+
 for i in range(n):
+    if (i != 0):
+        if (directs[i] == directs[i-1]):
+            # print(f"변경 전 : now = {now}, ans[now] = {ans[now]}")
+            ans[now] -= 1
+            # print(f"변경 후 :now = {now}, ans[now] = {ans[now]}")
+
     if (directs[i] == 'L'):
         for j in range(now, now-x[i]-1, -1):
             ans[j] += 1
@@ -20,11 +26,12 @@ for i in range(n):
             ans[j] += 1
         now += x[i]
     # print(f"i = {i}, x[i] = {x[i]}, now = {now}")
-
+# print(ans)
 cnt = 0
 for i in range(len(ans)):
     if (ans[i]>=2) & (ans[i+1]>=2):
         cnt += 1
+        # print(f"ans[{i}] = {ans[i]}, cnt = {cnt}")
     if (ans[i] == 0) & (cnt > 0):
         break
 print(cnt)
