77f54465f5735364ac3c44bd6fd7e1f51bd96584
SE <68493088+soeun0615@users.noreply.github.com>
2025-06-03 23:28:42 +0900

    Title: 겹치지 않는 사각형의 넓이, Time: 197ms, Memory: 48MB, Status: Passed - Codetree

--- 250603/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.py
+++ 250603/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.py
@@ -1,20 +1,22 @@
-ans = [[0]*1000 for _ in range(1000)]
+ans = [[0]*2000 for _ in range(2000)]
 
 for _ in range(2):
     x1, y1, x2, y2 = map(int, input().split())
+    x1, y1, x2, y2 = x1+1000, y1+1000, x2+1000, y2+1000
     for i in range(x1, x2):
         for j in range(y1, y2):
             ans[i][j] = 1
 
 # m넓이만큼을 빼기
 x1, y1, x2, y2 = map(int, input().split())
+x1, y1, x2, y2 = x1+1000, y1+1000, x2+1000, y2+1000
 for i in range(x1, x2):
     for j in range(y1, y2):
         ans[i][j] = 0
 
 width = 0
-for i in range(1000):
-    for j in range(1000):
+for i in range(2000):
+    for j in range(2000):
         if (ans[i][j] == 1):
             width += 1
 
